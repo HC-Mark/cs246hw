16788209a90ef39b260d52730a2092d9021efdb3
Tianming Xu <txu1@haverford.edu>
2017-01-20 15:14:00 -0500

    Add files via upload

--- calc.c
+++ calc.c
@@ -1,7 +1,7 @@
 /* calc.c
  *
  * Name:Tianming Xu
- * Desc:a not so simple c program that fulfill a simple calculator.
+ * Desc:a not so simple c program that works as a simple calculator.
  */
 
 #include <stdio.h>
@@ -23,61 +23,56 @@ int main(){
   int num1, num2, result;
   
   while(42){
-      printf("test\n");
       printf("Enter an operation: ");
-      scanf("%c", &op_input);
+      scanf(" %c", &op_input);
       if(op_input == '0'){
 	break;
       }
-      //if the operation we get is '+'
-      else if(op_input == '+'){
-	//get two inputs
+
+      	//get two inputs
 	printf("Enter a number: ");
 	scanf("%d", &num1);
 
 	printf("Enter a number: ");
 	scanf("%d", &num2);
-
+      //if the operation we get is '+'
+      if(op_input == '+'){
+	
 	result = num1 + num2;
 	printf("%d + %d = %d \n", num1, num2, result);
+	
       }
       //if the operation we get is '-'
       else if(op_input == '-'){
-	//get two inputs
-	printf("Enter a number: ");
-	scanf("%d", &num1);
-
-	printf("Enter a number: ");
-	scanf("%d", &num2);
-
+	
 	result = num1 - num2;
 	printf("%d - %d = %d \n", num1, num2, result);
+	
       }
 
       //if the operation we get is '*'
       else if(op_input == '*'){
-	//get two inputs
-	printf("Enter a number: ");
-	scanf("%d", &num1);
-
-	printf("Enter a number: ");
-	scanf("%d", &num2);
-
+	
 	result = num1 * num2;
 	printf("%d * %d = %d \n", num1, num2, result);
+	
       }
       //if the operation we get is '/'
       else if(op_input == '/'){
-	//get two inputs
-	printf("Enter a number: ");
-	scanf("%d", &num1);
-
-	printf("Enter a number: ");
-	scanf("%d", &num2);
-
+	if(num2 == 0){
+	  printf("invalid calculation, please do not divide by 0.\n");
+	  continue;
+	}
 	result = num1 / num2;
 	printf("%d / %d = %d \n", num1, num2, result);
+	
       }
+      else{
+	
+	printf("wrong type of input, please type '+','-','*','/',or'0' to exit program. ");
+	
+      }
+	
   
   }
 
